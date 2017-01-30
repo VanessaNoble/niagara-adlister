f5d740314962b800b9c1f4bbab7e7306aff54738
vanessamnoble <vanessamnoble@icloud.com>
2017-01-30 12:30:07 -0600

    Hello World

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,29 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
 
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    protected void doGet(
+            HttpServletRequest request,
+            HttpServletResponse response
+        )throws IOException {
 
+            response.setContentType("text/html"); //default
+
+            PrintWriter writer = response.getWriter();
+
+            String name = request.getParameter("name");
+
+            if (name == null) {
+                writer.println("<h1>Hello" + name + "</h1>");
+            } else {
+                writer.println("<h1>Hello World </h1>");
+        }
     }
 }
